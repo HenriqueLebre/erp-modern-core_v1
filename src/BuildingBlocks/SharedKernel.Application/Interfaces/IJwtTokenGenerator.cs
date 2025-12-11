1d6698cb24e3c976f90549838e87df90717f47a3
henriqueLebre <henriquelebredev@hotmail.com>
2025-12-11 08:03:26 -0300

    implements Interface for JWT

---
+++ src/BuildingBlocks/SharedKernel.Application/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedKernel.Application.Interfaces;
+
+/// <summary>
+/// Serviço de geração de token JWT (será implementado na camada Infrastructure).
+/// </summary>
+public interface IJwtTokenGenerator
+{
+    string GenerateToken(Guid userId, string username, string role);
+}

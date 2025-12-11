24577e28ab4a0ffeba6b563040c0d3f01e43cd3f
henriqueLebre <henriquelebredev@hotmail.com>
2025-12-11 07:58:54 -0300

    implements entity class for Password

---
+++ src/Modules/Auth/Auth.Domain/Interfaces/IPasswordHash.cs
@@ -0,0 +1,11 @@
+﻿namespace Auth.Domain.Interfaces;
+
+/// <summary>
+/// Serviço de hash de senha (será implementado em Infrastructure).
+/// </summary>
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string hashedPassword, string providedPassword);
+}
